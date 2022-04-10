ee16eec1f35219846809cc45178bb13daeaef42e
Jorge Fernandez <jorgeraulfernandez@gmail.com>
2022-04-10 17:06:51 -0300

    Cambios en la configuracion de wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-  mode: "development",
+  mode: "production",
   optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin()],
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  output: {
+    filename: "main.[contenthash].js",
+    clean: true,
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       {
         test: /\.css$/,
         exclude: /styles\.css$/,
@@ -20,35 +34,34 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: false },
-          },
-        ],
+        loader: "html-loader",
+        options: { minimize: true, sources: false },
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svs|jpg|gis)$/,
         use: [
           {
             loader: "file-loader",
             options: {
               esModule: false,
-              name: "assets/[name].[ext]",
             },
           },
         ],
       },
     ],
   },
+
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: "[name].[contenthash].css",
       ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets/" }],
     }),
   ],
 };
