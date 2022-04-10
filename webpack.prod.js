0681be8876e4119c72459bff063cb5fd73a1b47f
Jorge Fernandez <jorgeraulfernandez@gmail.com>
2022-04-10 17:07:30 -0300

    Cambios en la configuracion de wp

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,76 +1,53 @@
-const HtmlWebPackPlugin       = require('html-webpack-plugin'); 
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const MinifyPlugin            = require('babel-minify-webpack-plugin');
-const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    mode: 'production',
-    optimization: {
-        minimizer: [ new OptimizeCssAssetsPlugin() ]
-    },
-    output: {
-        filename: 'main.[contentHash].js'
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                use: [
-                    'babel-loader'
-                ]
+  mode: "development",
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /styles\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /styles\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: { minimize: false, sources: false },
+      },
+      {
+        test: /\.(png|svs|jpg|gis)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
             },
-            {
-                test: /\.css$/,
-                exclude: /styles\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /styles\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: { minimize: false }
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            esModule: false,
-                            name: 'assets/[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contentHash].css',
-            ignoreOrder: false
-        }),
-        new MinifyPlugin(),
-        new CleanWebpackPlugin(),
-    ]
+          },
+        ],
+      },
+    ],
+  },
 
-}
-
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets/" }],
+    }),
+  ],
+};
